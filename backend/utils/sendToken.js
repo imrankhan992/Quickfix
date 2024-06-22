5cd98bc779d7ef49eb9d9cb687d324d78983a67a
imrankhan992 <imrankohistani65@gmail.com>
2024-06-22 19:02:15 +0500

    https only cookie problem

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -5,8 +5,8 @@ exports.sendToken = async (user, StatusCode, res) => {
     const options = {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        secure: false,// False to Allow both HTTP and HTTPS
-        sameSite: 'Lax', // Lax to allow CSRF protection by default and strict to disallow CSRF protection and None to disable CSRF protection
+
+       
     }
     res.status(StatusCode).cookie("token", token, options).json({
         success: true,
