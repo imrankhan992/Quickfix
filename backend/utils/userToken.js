a0a41a3dcc2739e00a427111ad63b3761cd06472
imrankhan992 <imrankohistani65@gmail.com>
2024-06-22 20:29:51 +0500

    cookie

--- backend/utils/userToken.js
+++ backend/utils/userToken.js
@@ -4,7 +4,8 @@ exports.userToken = async (user, StatusCode, res) => {
    
     const options = {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-        httpOnly: true
+        httpOnly: true,
+        secure: true,
     }
     res.status(StatusCode).cookie("usertoken", token, options).json({
         success: true,
