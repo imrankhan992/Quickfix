f535c783cffff75eddd352abbe3048c7c49d04e8
imrankhan992 <imrankohistani65@gmail.com>
2024-06-22 22:16:10 +0500

    url changes

--- backend/app.js
+++ backend/app.js
@@ -14,11 +14,15 @@ const { Server } = require("socket.io");
 const http = require("http");
 
 const server = http.createServer(app);
-
+const allowedOrigins = [
+    "http://localhost:5173", // Local development
+    "https://quickfix-281be.web.app", // Production,
+    "https://noahai.ai"
+];
 const io = new Server(server, {
     cors: {
-        // origin: "http://localhost:5173",
-        origin: "https://quickfix-281be.web.app",
+        origin: allowedOrigins,
+        // origin: "https://quickfix-281be.web.app",
         methods: ["GET", "POST"],
         credentials: true,
     },
@@ -26,7 +30,7 @@ const io = new Server(server, {
 
 app.use(express.json());
 app.use(cookieParser());
-const allowedOrigins = ["https://quickfix-281be.web.app"];
+
 
 app.use(
     cors({
