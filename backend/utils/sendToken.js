1446d7d76e16c2be77c4b65b6f93e8651dac9f1d
imrankhan992 <imrankohistani65@gmail.com>
2024-06-22 21:41:09 +0500

    change cookie options

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -5,12 +5,13 @@ exports.sendToken = async (user, StatusCode, res) => {
     const options = {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        // cookie not store i am usng both http and https
-        secure: true,
-        
-// 
-        
-       
+        // secure: true,
+        secure: false,// False to Allow both HTTP and HTTPS
+        sameSite: 'none', // Lax to allow CSRF protection by default and strict to disallow CSRF protection and None to disable CSRF protection
+
+        // 
+
+
     }
     res.status(StatusCode).cookie("token", token, options).json({
         success: true,
--- backend/utils/userToken.js
+++ backend/utils/userToken.js
@@ -4,9 +4,11 @@ exports.userToken = async (user, StatusCode, res) => {
    
     const options = {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        
         httpOnly: true,
-        secure: true,
-        // 
+    // secure: true,
+    secure: false,// False to Allow both HTTP and HTTPS
+    sameSite: 'none',
     }
     res.status(StatusCode).cookie("usertoken", token, options).json({
         success: true,
