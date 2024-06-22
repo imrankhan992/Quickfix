39ea9f194e2c7363927c72bf94fb64bd9bdae1e9
imrankhan992 <imrankohistani65@gmail.com>
2024-06-22 18:40:58 +0500

    cookie settings

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -1,10 +1,12 @@
 
 exports.sendToken = async (user, StatusCode, res) => {
     const token = await user.getJwtToken();
-   
+
     const options = {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-        httpOnly: true
+        httpOnly: true,
+        secure: false,// False to Allow both HTTP and HTTPS
+        sameSite: 'Lax', // Lax to allow CSRF protection by default and strict to disallow CSRF protection and None to disable CSRF protection
     }
     res.status(StatusCode).cookie("token", token, options).json({
         success: true,
