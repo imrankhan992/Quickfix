a07c8a1d625232ff9c00e7272b8e4f9284d478cf
imrankhan992 <imrankohistani65@gmail.com>
2024-06-22 20:04:26 +0500

    cookie update

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -5,7 +5,11 @@ exports.sendToken = async (user, StatusCode, res) => {
     const options = {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true,
+        // cookie not store i am usng both http and https
+        secure: true,
+        
 
+        
        
     }
     res.status(StatusCode).cookie("token", token, options).json({
